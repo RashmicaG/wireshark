b6c7d239dd135493db8b4716e790bdacdaaead88
Rashmica Gupta <rashmica@linux.ibm.com>
2022-09-26 12:20:14 +1000

    Add some PLDM BIOS Commands
    DSP0247 - Platform Level Data Model (PLDM) for BIOS
    Control and Configuration Specification
    
    Signed-off-by: Rashmica Gupta <rashmica@linux.ibm.com>

---
+++ epan/dissectors/packet-pldm-bios.c
@@ -0,0 +1,210 @@
+#include <stdint.h>
+#include "packet-pldm-base.h"
+
+static int proto_pldm_bios=-1;
+static int hf_pldm_cmd=-1;
+static int hf_completion_code=-1;
+static int hf_bios_data_handle=-1;
+static int hf_bios_transfer_op_flag=-1;
+static int hf_bios_table_type=-1;
+static int hf_bios_next_data_handle=-1;
+static int hf_bios_transfer_flag=-1;
+static int hf_bios_attr_handle=-1;
+
+static const value_string pldm_cmds[] ={
+    {0x01, "GetBIOSTable"},
+    {0x02, "SetBIOSTable"},
+    {0x03, "UpdateBIOSTable"},
+    {0x04, "GetBIOSTableTags"},
+    {0x05, "SetBIOSTableTags"},
+    {0x06, "AcceptBIOSAttributesPending"},
+    {0x07, "SetBIOSAttributeCurrentValue"},
+    {0x08, "GetBIOSAttributeCurrentValueByHandle"},
+    {0x09, "GetBIOSAttributePendingValueByHandle"},
+    {0x0a,  "GetBIOSAttributeCurrentValueByType"},
+    {0x0b,  "GetBIOSAttributePendingValueByType"},
+    {0x0c,  "GetDateTime"},
+    {0x0d,  "SetDateTime"},
+    {0x0e,  "GetBIOSStringTableStringType"},
+    {0x0f,  "SetBIOSStringTableStringType"},
+    {0, NULL}
+};
+
+static const value_string completion_codes[]={
+    {0x0, "Success"},
+    {0x1, "Error"},
+    {0x2, "Invalid Data"},
+    {0x3, "Invalid Length"},
+    {0x4, "Not Ready"},
+    {0x5, "Unsupported PLDM command"},
+    {0x20, "Invalid PLDM type"},
+    {0x80, "Invalid data transfer handle"},
+    {0x81, "Invalid transfer operation flag"},
+    {0x82, "Invalid transfer flag"},
+    {0x83, "BIOS table unavailable"},
+    {0x84, "Invalid BIOS table integrity check"},
+    {0x85, "Invalid BIOS table"},
+    {0x86, "BIOS table tag unavailable"},
+    {0x87, "Invalid BIOS table tag type"},
+    {0x88, "Invalid BIOS attr handle"},
+    {0x89, "Invalid BIOS attr type"},
+    {0x8a, "No date time info available"},
+    {0x8b, "Invalid string type"},
+    {0, NULL}
+};
+
+static const value_string bios_table_types[] ={
+    {0x0,   "BIOS String Table"},
+    {0x1,   "BIOS Attribute Table"},
+    {0x2,   "BIOS Attribute Value Table"},
+    {0x3,   "BIOS Attribute Pending Value Table"},
+    {0, NULL}
+};
+
+static const value_string transfer_op_flags[] ={
+    {0x0,   "Get Next Part"},
+    {0x1,   "Get First Part"},
+    {0, NULL}
+};
+
+static const value_string transfer_flags[] ={
+    {0x1,   "Start"},
+    {0x2,   "Middle"},
+    {0x4,   "End"},
+    {0x5,   "Start and End"},
+    {0, NULL}
+};
+
+int
+dissect_bios(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *p_tree, void* data)
+{
+    struct packet_data *d = (struct packet_data*) data;
+    guint8 request = d->direction;
+    guint8 pldm_cmd = tvb_get_guint8(tvb, 0);
+    guint8 offset = 0;
+    proto_tree_add_item(p_tree, hf_pldm_cmd, tvb, offset, 1, ENC_LITTLE_ENDIAN);
+    offset +=1;
+    if (!request){
+        proto_tree_add_item(p_tree, hf_completion_code, tvb, 1, 1, ENC_LITTLE_ENDIAN);
+        guint8 completion_code = tvb_get_guint8(tvb, offset);
+        if (completion_code)
+            return tvb_captured_length(tvb);
+        offset +=1;
+    }
+    switch(pldm_cmd){
+    case 0x1: //Get BIOS Table
+        if (request){
+            proto_tree_add_item(p_tree, hf_bios_data_handle, tvb, offset, 4, ENC_LITTLE_ENDIAN);
+            offset +=4;
+            proto_tree_add_item(p_tree, hf_bios_transfer_op_flag, tvb, offset, 1, ENC_LITTLE_ENDIAN);
+            offset +=1;
+            proto_tree_add_item(p_tree, hf_bios_table_type, tvb, offset, 1, ENC_LITTLE_ENDIAN);
+        } else {
+            proto_tree_add_item(p_tree, hf_bios_next_data_handle, tvb, offset, 4, ENC_LITTLE_ENDIAN);
+            offset +=4;
+            proto_tree_add_item(p_tree, hf_bios_transfer_flag, tvb, offset, 1, ENC_LITTLE_ENDIAN);
+        }
+        break;
+    case 0x07: //Set BIOS Attribute Current Value by Handle
+        if (request){
+            proto_tree_add_item(p_tree, hf_bios_data_handle, tvb, offset, 4, ENC_LITTLE_ENDIAN);
+            offset +=4;
+            proto_tree_add_item(p_tree, hf_bios_transfer_flag, tvb, offset, 1, ENC_LITTLE_ENDIAN);
+        } else {
+            proto_tree_add_item(p_tree, hf_bios_next_data_handle, tvb, offset, 4, ENC_LITTLE_ENDIAN);
+        }
+        break;
+    case 0x08: //Get BIOS Attribute Current Value by Handle
+        if (request){
+            proto_tree_add_item(p_tree, hf_bios_data_handle, tvb, offset, 4, ENC_LITTLE_ENDIAN);
+            offset +=4;
+            proto_tree_add_item(p_tree, hf_bios_transfer_op_flag, tvb, offset, 1, ENC_LITTLE_ENDIAN);
+            offset +=1;
+            proto_tree_add_item(p_tree, hf_bios_attr_handle, tvb, offset, 2, ENC_LITTLE_ENDIAN);
+        } else {
+            proto_tree_add_item(p_tree, hf_bios_next_data_handle, tvb, offset, 4, ENC_LITTLE_ENDIAN);
+            offset +=4;
+            proto_tree_add_item(p_tree, hf_bios_transfer_flag, tvb, offset, 1, ENC_LITTLE_ENDIAN);
+        }
+        break;
+    case 0x0c: //Get Data and Time
+        break;
+    default:
+        col_append_fstr(pinfo->cinfo, COL_INFO, "Unsupported or Invalid PLDM command");
+        g_print("Invalid PLDM bios cmd %x \n", pldm_cmd);
+        break;
+
+    }
+    return tvb_captured_length(tvb);
+}
+
+void
+proto_register_bios(void)
+{
+    static hf_register_info hf[] ={
+        { &hf_bios_data_handle,{
+            "Data transfer handle", "pldm.bios.table.handle",
+            FT_UINT32, BASE_HEX,
+            NULL, 0x0,
+            NULL, HFILL}
+         },
+        { &hf_bios_transfer_op_flag,{
+            "Data transfer operation flag", "pldm.bios.table.opflag",
+            FT_UINT8, BASE_HEX,
+            VALS(transfer_op_flags), 0x0,
+            NULL, HFILL}
+         },
+        { &hf_bios_table_type,{
+            "BIOS table type", "pldm.bios.table.type",
+            FT_UINT8, BASE_HEX,
+            VALS(bios_table_types), 0x0,
+            NULL, HFILL}
+         },
+        { &hf_bios_next_data_handle,{
+            "Next data transfer handle", "pldm.bios.table.nexthandle",
+            FT_UINT32, BASE_HEX,
+            NULL, 0x0,
+            NULL, HFILL}
+         },
+        { &hf_bios_transfer_flag,{
+            "Data transfer operation flag", "pldm.bios.table.flag",
+            FT_UINT8, BASE_HEX,
+            VALS(transfer_flags), 0x0,
+            NULL, HFILL}
+         },
+        { &hf_bios_attr_handle,{
+            "Attribute handle", "pldm.bios.attr.handle",
+            FT_UINT16, BASE_HEX,
+            NULL, 0x0,
+            NULL, HFILL}
+         },
+        { &hf_pldm_cmd,{
+            "PLDM Command Type", "pldm.cmd",
+            FT_UINT8, BASE_HEX,
+            VALS(pldm_cmds), 0x0,
+            NULL, HFILL}
+         },
+         { &hf_completion_code,{
+            "Completion Code", "pldm.cc",
+            FT_UINT8, BASE_DEC,
+            VALS(completion_codes), 0x0,
+            NULL, HFILL}
+         },
+    };
+
+    proto_pldm_bios = proto_register_protocol (
+        "PLDM BIOS Control and Configuration", /* name        */
+        "PLDM_bios",          /* short_name  */
+        "pldm.bios"           /* filter_name */
+        );
+    proto_register_field_array(proto_pldm_bios, hf, array_length(hf));
+}
+
+void
+proto_reg_handoff_bios(void)
+{
+    static dissector_handle_t bios_handle;
+   
+    bios_handle = create_dissector_handle(dissect_bios, proto_pldm_bios);
+    dissector_add_uint("pldm.type", PLDM_BIOS, bios_handle);
+}
